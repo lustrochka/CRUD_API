1bb62747919fe795020aa965b266eb479388fd25
Olga <kartoshkaslukom@gmail.com>
2024-10-16 00:58:37 +0200

    feat: add get user request

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-import { users } from './users';
+import { users, getUser } from './users';
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +8,11 @@ http.createServer((req, res) => {
     if (url === '/api/users' && method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(users));
+      } else if (url?.startsWith("/api/users/")) {
+        const id = url.split('/')[3];
+        const { code, message, contentType } = getUser(id);
+        res.writeHead(code, { 'Content-Type': contentType });
+        res.end(message);
       }
 }).listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/users.ts
+++ src/users.ts
@@ -1,12 +1,31 @@
-export const users = [{
-    id: '1',
+import { validate, v4 } from 'uuid';
+
+type user = {
+    id: string,
+    username: string,
+    age: number,
+    hobbies: string[]
+}
+
+export const users: user[] = [{
+    id: '123e4567-e89b-12d3-a456-426614174000',
     username: 'aaa',
     age: 5,
     hobbies: ['crying', 'eating']
 },
 {
-    id: '2',
+    id: 'c9c9e90e-60c3-4c57-8c54-6c26bc6ab0c3',
     username: 'Jane',
     age: 20,
     hobbies: ['painting']
 }]
+
+export function getUser(id: string) {
+    if (!validate(id)) return {code: 400, message: "Invalid id", contentType: 'text/plain'};
+
+    const user = users.find((user) => user.id === id);
+
+    if (!user) return {code: 404, message: "User doesn't exist", contentType: 'text/plain'};
+
+    return {code: 200, message: JSON.stringify(user), contentType: 'application/json'};
+}
