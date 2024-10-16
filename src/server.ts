ee0b28a8f06877c85bf11126cbeea4522f90517f
Olga <kartoshkaslukom@gmail.com>
2024-10-16 16:06:29 +0200

    feat: add put user

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 import http from 'http';
 import { users } from './users';
-import { addUser } from './addUser';
+import { postUser } from './postUser';
 import { getUser } from './getUser';
+import { putUser } from './putUser';
 
 const PORT = process.env.PORT || 3000;
+const baseUrl = '/api/users';
 
 http
   .createServer((req, res) => {
     const { method, url } = req;
-    if (url === '/api/users') {
-      if (method === 'GET') {
+
+    if (!url) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Invalid URL');
+  } else {
+    switch(true) {
+      case url === baseUrl && method === 'GET': {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(users));
-      } else if (method === 'POST') {
+        break;
+      }
+
+      case url === baseUrl && method === 'POST': {
         let body = '';
 
         req.on('data', (chunk) => {
@@ -20,18 +30,39 @@ http
         });
 
         req.on('end', () => {
-          const { code, message, contentType } = addUser(body); 
-          
+          const { code, message, contentType } = postUser(body);
+
+          res.writeHead(code, { 'Content-Type': contentType });
+          res.end(message);
+        });
+        break;
+      }
+
+      case url?.startsWith(baseUrl + '/') && method === 'GET': {
+        const id = url.split('/')[3];
+        const { code, message, contentType } = getUser(id);
+        res.writeHead(code, { 'Content-Type': contentType });
+        res.end(message);
+        break;
+      }
+
+      case url?.startsWith(baseUrl + '/') && method === 'PUT': {
+        let body = '';
+        const id = url.split('/')[3];
+
+        req.on('data', (chunk) => {
+          body += chunk.toString();
+        });
+
+        req.on('end', () => {
+          const { code, message, contentType } = putUser(id, body);
+
           res.writeHead(code, { 'Content-Type': contentType });
           res.end(message);
         });
       }
-    } else if (url?.startsWith('/api/users/')) {
-      const id = url.split('/')[3];
-      const { code, message, contentType } = getUser(id);
-      res.writeHead(code, { 'Content-Type': contentType });
-      res.end(message);
     }
+  }
   })
   .listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/users.ts
+++ src/users.ts
@@ -20,3 +20,15 @@ export const users: User[] = [
   },
 ];
 
+export function isUser(body: Partial<User>) {
+  const { username, age, hobbies } = body;
+  return (
+    username &&
+    typeof username === 'string' &&
+    age &&
+    typeof age === 'number' &&
+    hobbies &&
+    Array.isArray(hobbies) &&
+    hobbies.every((hobby) => typeof hobby === 'string')
+  );
+}
